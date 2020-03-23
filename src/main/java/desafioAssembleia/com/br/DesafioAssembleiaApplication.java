0a28990bc67bb5d41ea1583f0581f3c721524ad1
KarenF <karenf@outlook.com>
2020-03-23 10:14:58 -0300

    add project

---
+++ src/main/java/desafioAssembleia/com/br/DesafioAssembleiaApplication.java
@@ -0,0 +1,13 @@
+package desafioAssembleia.com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAssembleiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAssembleiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafioAssembleia/com/br/DesafioAssembleiaApplicationTests.java
@@ -0,0 +1,13 @@
+package desafioAssembleia.com.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAssembleiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
