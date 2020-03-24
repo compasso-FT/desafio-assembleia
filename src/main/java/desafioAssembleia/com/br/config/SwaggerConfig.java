97426a19adeac7aa7a36db884acd02cdb2887d74
KarenF <karenf@outlook.com>
2020-03-24 15:57:36 -0300

    <@KarenF> add teste controller

--- src/main/java/desafioAssembleia/com/br/config/SwaggerConfig.java
+++ src/main/java/desafioAssembleia/com/br/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.techprimers.springboot.swaggerexample"))
+                .apis(RequestHandlerSelectors.basePackage("desafioAssembleia.com.br"))
                 .paths(regex("/.*"))
                 .build();
     }
