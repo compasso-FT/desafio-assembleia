31a4c327f2c801ddc5c465cece90417393d00154
KarenF <karenf@outlook.com>
2020-03-30 17:59:17 -0300

    <@KarenF> add @Repository

--- src/main/java/br/com/desafioAssembleia/repository/AssociadoRepository.java
+++ src/main/java/br/com/desafioAssembleia/repository/AssociadoRepository.java
@@ -1,9 +1,11 @@
 package br.com.desafioAssembleia.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.desafioAssembleia.entity.Associado;
 
+@Repository
 public interface AssociadoRepository extends JpaRepository<Associado, Long> {
 
 }
