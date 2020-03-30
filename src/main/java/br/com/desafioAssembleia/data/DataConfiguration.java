d5beb216273c8ef66c39328fdfaa2cdd8a3a0818
KarenF <karenf@outlook.com>
2020-03-30 11:51:41 -0300

    <@KarenF> add data configuration

---
+++ src/main/java/br/com/desafioAssembleia/data/DataConfiguration.java
@@ -0,0 +1,43 @@
+package br.com.desafioAssembleia.data;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+@Profile("dev")
+public class DataConfiguration {
+
+	@Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+
+        factoryBean.setJpaVendorAdapter(vendorAdapter);
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("123456");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/desafio_assembleia");
+        dataSource.setDriverClassName("org.postgresql.Driver");
+
+        factoryBean.setDataSource(dataSource);
+
+        Properties props = new Properties();
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        props.setProperty("hibernate.show_sql", "true");
+        props.setProperty("hibernate.hbm2ddl.auto", "update");
+
+        factoryBean.setJpaProperties(props);
+
+        factoryBean.setPackagesToScan("br.com.desafioAssembleia.entity");
+
+        return factoryBean;
+    }
+}
